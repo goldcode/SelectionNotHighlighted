b91051a6a6d56f7a89bd68caa752c75ed44c1cfd
SYNENTEC\RDsa <r.dsa@synentec.com>
2023-01-07 01:42:52 +0100

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var Persons = new ObservableCollection<Person>();
+            Persons.Add(new Person { Name = "Peter" });
+            Persons.Add(new Person { Name = "Tom" });
+            Persons.Add(new Person { Name = "Jane" });
+
+
+            var selectedPersons = new ObservableCollection<Person>();
+
+
+            DataContext = new ViewModel(Persons, selectedPersons);
+            selectedPersons.Add(Persons.ElementAt(0));
+        }
+    }
+}
---
+++ WpfApp3/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class Person
+    {
+        public string Name { get; set; } = string.Empty;
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class ViewModel
+    {
+        public ObservableCollection<Person> PersonViewModels { get; set; } = new ();
+
+        public ObservableCollection<Person> SelectedPersonViewModels { get; set; } = new();
+
+
+        public ViewModel(ObservableCollection<Person> personViewModels, ObservableCollection<Person> selectedPersonViewModels)
+        {
+            PersonViewModels = personViewModels;
+            SelectedPersonViewModels = selectedPersonViewModels;
+        }
+    }
+}
